e9c23e32dea4c085a2abda9f6f5c17545d9a8bec
EccentricHarry <harinathrao13@gmail.com>
2024-02-19 08:50:04 +0530

    ..

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
       <div className="w-full h-screen duration-200 " style={{backgroundColor: color}} >
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-          <div className="flex flexwrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <div className=" flex flexwrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
             <button 
             onClick={()=> setColor("red")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"red"}}>Red</button>
             <button 
@@ -18,7 +18,7 @@ function App() {
             <button 
             onClick={()=> setColor("lavender")}className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"lavender"}}>Lavender</button>
             <button 
-            onClick={()=> setColor("purple")}className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"purple"}}>Purple</button>
+            onClick={()=> setColor("purple")}className="outline-10 outline-white px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"purple"}}>Purple</button>
           </div>
         </div>
       </div>
