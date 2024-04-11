b8564ed0da4c2ec20595bf3700b65f966ee681cc
EccentricHarry <harinathrao13@gmail.com>
2024-04-11 12:48:45 +0530

    MegaBlog

---
+++ 12MegaBlog/src/Appwrite/auth.js
@@ -0,0 +1,59 @@
+import conf from '..Conf/conf.js';
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService{
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, password, name}){
+        try {
+            const userAccount = await this.account.create(ID.unique() ,email, password, name); // we can give as many parameters as we need!
+            if(userAccount){
+                // return another function
+                return this.login({email, password});
+            } else {
+                return userAccount;
+            }
+            
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}){
+        try {
+           return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return this.account.get();
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error: " ,error);
+        }
+        return null;
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+           console.log("Appwrite service :: logout :: error: " ,error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService; 
---
+++ 12MegaBlog/src/Appwrite/config.js
@@ -0,0 +1,40 @@
+import conf from '..Conf/conf.js';
+import {Client , Databases, Storage, Query} from "appwrite";
+
+export class Service {
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId, 
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite servise :: createPost :: error:" , error);
+        }
+    }
+
+
+}
+
+const service = new Service();
+export default service;
