c42ece9c7c7bf0dbd727ec94dd6a871d1d3f51c6
EccentricHarry <harinathrao13@gmail.com>
2024-04-19 23:02:53 +0530

    React Components

---
+++ 12MegaBlog/src/Components/Select.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useId } from 'react'
+
+
+function Select({
+    options, // options are by default arrays;
+    lable,
+    className = '',
+    ...props
+}, ref) {
+    const id = useId();
+  return (
+    <div className='w-full'>
+        {lable && <lable htmlFor={id} className=''>{lable}</lable>}
+        <select
+        {...props}
+        id = {id}
+        ref = {ref}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+        >
+            {options?.map((option) =>
+        <option key={option} value={option}>{option}</option>)}
+        </select>
+    </div>
+  )
+}
+
+export default React.forwardRef(Select)
